903429651574076a9529a13c18450f3367a21f07
liuheng <liuheng@yidian-inc.com>
2019-02-18 17:59:55 +0800

    add server support for deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 pwd
 ls
-docker stop www-web
-docker rm www-web
-docker rmi react/www-web
+# docker stop www-web
+# docker rm www-web
+# docker rmi react/www-web
 docker build -t react/www-web .
 docker run -d --name www-web -p 3000:3000 react/www-web
